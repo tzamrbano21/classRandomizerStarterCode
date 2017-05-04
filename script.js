e2bbbe21215c7a53887034cfb1e9cc928a30e7ec
tonny zambrano <tonnyzambrano21@gmail.com>
2017-05-04 04:33:26 +0000

    make randomizer

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-
-
-
+var students=["Jennifer", "David", "Tonny", "Kevin", "Bradley"];
+$("#studentButton").click(function(){
+    var randomStudents=Math.floor(Math.random()*students.length);
+    $("#studentDisplay").html(students[randomStudents]);
+});
+    var teachers=["Aaron", "Alyxe", "Justin", "Julia", "Zach"];
+    $("#teacherButton).click(function(){
+        var randomTeachers=Math.floor(Math.random()*teachers.length);
+        $("#teacherDisplay").html(teachers[randomTeachers]);
+});
 });
